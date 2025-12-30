29c84af9b60491af83907d09f5e28dfa2ad989a0
tonguc <tkaracay@gmail.com>
2025-12-30 21:24:18 +0300

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,18 +1,16 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
-import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas'
 
 export default defineConfig({
   name: 'default',
   title: 'Tonguç Karaçay Blog',
 
-  projectId: 'YOUR_PROJECT_ID', // Sanity.io'dan alacağız
-  dataset: 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'YOUR_PROJECT_ID',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
 
   plugins: [
     structureTool(),
-    visionTool(),
   ],
 
   schema: {
